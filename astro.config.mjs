6e6202bdcedc15f5940e62ddd9ca06cf00a278e2
Willian Damasceno <williandamascenoprofessional@gmail.com>
2023-08-18 23:54:18 -0300

    Config - solid-js integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-
-import vercel from "@astrojs/vercel/edge";
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import vercel from "@astrojs/vercel/edge"
+import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), solidJs()],
   compressHTML: true,
   output: "hybrid",
   adapter: vercel({
     analytics: true,
   }),
-});
+})
