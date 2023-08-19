da1d62cb48ce0fe54af25a4f026f0e0cef46e357
Willian Damasceno <williandamascenoprofessional@gmail.com>
2023-08-19 17:19:11 -0300

    Config - Theme setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,21 @@
+function withOpacity(variable) {
+  return ({ opacityValue }) => {
+    if (opacityValue === undefined) {
+      return `hsl(var(${variable}))`
+    }
+    return `hsl(var(${variable}) / ${opacityValue})`
+  }
+}
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    colors: {
+      primary: withOpacity("--primary"),
+      base: {
+        100: withOpacity("--base-100"),
+      },
+    },
     extend: {
       container: {
         center: true,
@@ -23,10 +37,20 @@ module.exports = {
       {
         dark: {
           ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
-          primary: "orangered",
+          primary: "hsl(20 100% 55%)",
+          "--primary": "15 100% 55%",
+
+          "base-100": "hsl(225 25% 20%)",
+          "--base-100:": "225 25% 20%",
+
+          "base-200": "hsl(225 25% 15%)",
+          "--base-200:": "225 25% 15%",
+
+          "base-300": "hsl(225 25% 10%)",
+          "--base-300:": "225 25% 10%",
         },
       },
     ],
-    logs: false
+    logs: false,
   },
 }
