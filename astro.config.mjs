ca90ccc416b9104b640b36eae6df1ebd92e4ed6f
Willian Damasceno <williandamascenoprofessional@gmail.com>
2023-08-18 23:25:27 -0300

    Config - Vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://example.com",
   integrations: [mdx(), sitemap(), tailwind()],
   compressHTML: true,
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    analytics: true,
+  }),
 });
