07e7efb9b73e8e2a0e1a92315449e5757e2d8bae
Willian Damasceno <williandamascenoprofessional@gmail.com>
2023-08-19 14:53:04 -0300

    Config - Config file initial setup

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE = {
+  title: "Willian Damasceno",
+  description: "Portfolio Website",
+  author: "Willian Damasceno",
+  ogImage: "",
+  theme: "dark",
+  locale: ["en-US"]
+}
