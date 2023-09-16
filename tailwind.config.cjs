aa72c85fa596aef70fbc7945c722b3429b685ebc
Willian Damasceno <williandamascenoprofessional@gmail.com>
2023-09-15 21:45:08 -0300

    Changing the base colors again

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,15 +11,7 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      colors: {
-        primary: withOpacity("--primary"),
-        base: {
-          100: withOpacity("--base-100"),
-          200: withOpacity("--base-200"),
-          300: withOpacity("--base-300"),
-          content: withOpacity("--base-content"),
-        },
-      },
+      colors: {},
       container: {
         center: true,
         padding: "1rem",
@@ -41,19 +33,11 @@ module.exports = {
         dark: {
           ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
           primary: "hsl(10 100% 55%)",
-          "--primary": "10 100% 55%",
 
-          "base-100": "hsl(215 25% 15%)",
-          "--base-100:": "215 25% 15%",
-
-          "base-200": "hsl(215 25% 10%)",
-          "--base-200:": "215 25% 10%",
-
-          "base-300": "hsl(215 25% 5%)",
-          "--base-300:": "215 25% 5%",
-
-          "base-content": "hsl(215 25% 80%)",
-          "--base-content": "215 15% 80%",
+          "base-100": "hsl(215 20% 14%)",
+          "base-200": "hsl(215 20% 12%)",
+          "base-300": "hsl(215 20% 10%)",
+          "base-content": "hsl(215 20% 85%)",
         },
       },
     ],
